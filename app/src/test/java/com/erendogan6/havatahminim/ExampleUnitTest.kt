001f867c5fc385d2c86425cf1b6bc2320e17d998
erendogan6 <erendogan6@hotmail.com>
2024-07-11 00:50:19 +0300

    chore: Initialize Android Studio project with default settings

---
+++ app/src/test/java/com/erendogan6/havatahminim/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package com.erendogan6.havatahminim
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
