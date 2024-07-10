8737cfdc34f17e312dcdb2b1b0882cbaf8b6cc9d
erendogan6 <erendogan6@hotmail.com>
2024-07-11 01:36:28 +0300

    feat(di): Add AppModule with HILT for dependency injection

---
+++ app/src/main/java/com/erendogan6/havatahminim/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.erendogan6.havatahminim.di
+import com.erendogan6.havatahminim.network.WeatherApiService
+import com.erendogan6.havatahminim.repository.WeatherRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherApiService(okHttpClient: OkHttpClient): WeatherApiService {
+        return Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherRepository(weatherApiService: WeatherApiService): WeatherRepository {
+        return WeatherRepository(weatherApiService)
+    }
+}
