da6a19007ca1ddfb2a61f5f420779eff41906bf6
erendogan6 <erendogan6@hotmail.com>
2024-07-11 14:18:58 +0300

    feat(ui): Add NetworkUtils and update MainActivity to check network availability

--- app/src/main/java/com/erendogan6/havatahminim/ui/view/MainActivity.kt
+++ app/src/main/java/com/erendogan6/havatahminim/ui/view/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.erendogan6.havatahminim.ui.view
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.erendogan6.havatahminim.ui.theme.HavaTahminimTheme
 import com.erendogan6.havatahminim.ui.viewModel.WeatherViewModel
+import com.erendogan6.havatahminim.util.NetworkUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,7 +16,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             HavaTahminimTheme {
                 val weatherViewModel: WeatherViewModel = hiltViewModel()
-                weatherViewModel.fetchWeather(41.0448307, 28.9743697, "3d4e2ea2d92e6ec224c1bc97c4057c27") // Doğru API anahtarını buraya ekleyin
+                if (NetworkUtils.isNetworkAvailable(this)) {
+                    weatherViewModel.fetchWeather(41.0448307, 28.9743697, "3d4e2ea2d92e6ec224c1bc97c4057c27")
+                } else {
+                    // Handle no internet connection
+                }
                 WeatherScreen(weatherViewModel)
             }
         }
---
+++ app/src/main/java/com/erendogan6/havatahminim/util/isNetworkAvailable.kt
@@ -0,0 +1,14 @@
+package com.erendogan6.havatahminim.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+object NetworkUtils {
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+        return networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+}
