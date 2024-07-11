43c2e14369cadd2679d156cf5326ad7723691186
erendogan6 <erendogan6@hotmail.com>
2024-07-11 14:19:50 +0300

    feat(ui): Add error and loading states to WeatherScreen

--- app/src/main/java/com/erendogan6/havatahminim/ui/view/WeatherScreen.kt
+++ app/src/main/java/com/erendogan6/havatahminim/ui/view/WeatherScreen.kt
@@ -2,6 +2,7 @@ package com.erendogan6.havatahminim.ui.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,16 +15,24 @@ import com.erendogan6.havatahminim.ui.viewModel.WeatherViewModel
 @Composable
 fun WeatherScreen(weatherViewModel: WeatherViewModel) {
     val weatherState = weatherViewModel.weatherState.collectAsState().value
+    val errorMessage = weatherViewModel.errorMessage.collectAsState().value
 
     Surface(color = MaterialTheme.colorScheme.background) {
         Column(modifier = Modifier.padding(16.dp)) {
-            weatherState?.let {
-                Text(text = "Durum: ${it.weather[0].description}")
-                Text(text = "Sıcaklık: ${it.main.temp}°C")
-                Text(text = "Hissedilen Sıcaklık: ${it.main.feels_like}°C")
-                Text(text = "Nem: ${it.main.humidity}%")
-            } ?: run {
-                Text(text = "Veri yükleniyor...")
+            when {
+                errorMessage != null -> {
+                    Text(text = errorMessage)
+                }
+                weatherState != null -> {
+                    Text(text = "Durum: ${weatherState.weather[0].description}")
+                    Text(text = "Sıcaklık: ${weatherState.main.temp}°C")
+                    Text(text = "Hissedilen Sıcaklık: ${weatherState.main.feels_like}°C")
+                    Text(text = "Nem: ${weatherState.main.humidity}%")
+                }
+                else -> {
+                    CircularProgressIndicator()
+                    Text(text = "Veri yükleniyor...")
+                }
             }
         }
     }
