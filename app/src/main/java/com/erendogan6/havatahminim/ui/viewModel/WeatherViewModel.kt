51b387518681f8ffc17f458c080393c1b8b32d38
erendogan6 <erendogan6@hotmail.com>
2024-07-11 14:19:24 +0300

    feat(viewmodel): Add error handling in WeatherViewModel

--- app/src/main/java/com/erendogan6/havatahminim/ui/viewModel/WeatherViewModel.kt
+++ app/src/main/java/com/erendogan6/havatahminim/ui/viewModel/WeatherViewModel.kt
@@ -19,13 +19,18 @@ class WeatherViewModel @Inject constructor(
     private val _weatherState = MutableStateFlow<BaseResponse?>(null)
     val weatherState: StateFlow<BaseResponse?> = _weatherState
 
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> = _errorMessage
+
     fun fetchWeather(lat: Double, lon: Double, apiKey: String) {
         viewModelScope.launch {
             try {
                 val response = repository.getWeather(lat, lon, apiKey)
                 _weatherState.value = response
+                _errorMessage.value = null
                 Log.d("WeatherViewModel", "Weather data fetched successfully")
             } catch (e: Exception) {
+                _errorMessage.value = "Veri yüklenemedi. Lütfen tekrar deneyin."
                 Log.e("WeatherViewModel", "Error fetching weather data", e)
             }
         }
