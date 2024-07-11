7233996d3737d55ac1d501560bc87e8d59434548
erendogan6 <erendogan6@hotmail.com>
2024-07-12 02:00:59 +0300

    feat(di): Add ProWeatherApiService for hourly forecast API

--- app/src/main/java/com/erendogan6/havatahminim/di/AppModule.kt
+++ app/src/main/java/com/erendogan6/havatahminim/di/AppModule.kt
@@ -1,4 +1,6 @@
 package com.erendogan6.havatahminim.di
+
+import com.erendogan6.havatahminim.network.ProWeatherApiService
 import com.erendogan6.havatahminim.network.WeatherApiService
 import com.erendogan6.havatahminim.repository.WeatherRepository
 import dagger.Module
@@ -39,7 +41,21 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideWeatherRepository(weatherApiService: WeatherApiService): WeatherRepository {
-        return WeatherRepository(weatherApiService)
+    fun provideProWeatherApiService(okHttpClient: OkHttpClient): ProWeatherApiService {
+        return Retrofit.Builder()
+            .baseUrl("https://pro.openweathermap.org/data/2.5/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ProWeatherApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherRepository(
+        weatherApiService: WeatherApiService,
+        proWeatherApiService: ProWeatherApiService
+    ): WeatherRepository {
+        return WeatherRepository(weatherApiService, proWeatherApiService)
     }
 }
