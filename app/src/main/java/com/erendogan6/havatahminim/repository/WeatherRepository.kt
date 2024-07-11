c405c132b447b11969d1da9b31e5a339ed567c30
erendogan6 <erendogan6@hotmail.com>
2024-07-12 02:02:53 +0300

    feat(repository): Add method for fetching hourly weather in WeatherRepository

--- app/src/main/java/com/erendogan6/havatahminim/repository/WeatherRepository.kt
+++ app/src/main/java/com/erendogan6/havatahminim/repository/WeatherRepository.kt
@@ -1,12 +1,20 @@
 package com.erendogan6.havatahminim.repository
-import com.erendogan6.havatahminim.model.BaseResponse
+
+import com.erendogan6.havatahminim.model.CurrentWeatherBaseResponse
+import com.erendogan6.havatahminim.model.HourlyForecastBaseResponse
+import com.erendogan6.havatahminim.network.ProWeatherApiService
 import com.erendogan6.havatahminim.network.WeatherApiService
 import javax.inject.Inject
 
 class WeatherRepository @Inject constructor(
-    private val weatherApiService: WeatherApiService
+    private val weatherApiService: WeatherApiService,
+    private val proWeatherApiService: ProWeatherApiService
 ) {
-    suspend fun getWeather(lat: Double, lon: Double, apiKey: String): BaseResponse {
+    suspend fun getWeather(lat: Double, lon: Double, apiKey: String): CurrentWeatherBaseResponse {
         return weatherApiService.getWeather(lat, lon, apiKey)
+    }
+
+    suspend fun getHourlyWeather(lat: Double, lon: Double, apiKey: String): HourlyForecastBaseResponse {
+        return proWeatherApiService.getHourlyWeather(lat, lon, apiKey)
     }
 }
