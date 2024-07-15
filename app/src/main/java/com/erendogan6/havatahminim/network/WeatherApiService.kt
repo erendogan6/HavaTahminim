36ddb6cc063589771c3a35c9260d4f0c613e4e26
erendogan6 <erendogan6@hotmail.com>
2024-07-15 20:43:13 +0300

    feat(api): Add getDailyWeather method to WeatherApiService

--- app/src/main/java/com/erendogan6/havatahminim/network/WeatherApiService.kt
+++ app/src/main/java/com/erendogan6/havatahminim/network/WeatherApiService.kt
@@ -1,6 +1,7 @@
 package com.erendogan6.havatahminim.network
 
 import com.erendogan6.havatahminim.model.CurrentWeatherBaseResponse
+import com.erendogan6.havatahminim.model.DailyForecastBaseResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -13,4 +14,14 @@ interface WeatherApiService {
         @Query("units") units: String = "metric",
         @Query("lang") lang: String = "tr"
     ): CurrentWeatherBaseResponse
+
+    @GET("forecast/daily")
+    suspend fun getDailyWeather(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("appid") apiKey: String,
+        @Query("cnt") days: Int = 7,
+        @Query("units") units: String = "metric",
+        @Query("lang") lang: String = "tr"
+    ): DailyForecastBaseResponse
 }
