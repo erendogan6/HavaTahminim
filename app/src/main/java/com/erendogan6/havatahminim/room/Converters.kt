12b46a5e265d7fd0d3e60f9fb8bdd8a1de7e142d
erendogan6 <erendogan6@hotmail.com>
2024-07-19 02:47:17 +0300

    feat(room): add Converters class to handle DailyForecastBaseResponse serialization for Room

---
+++ app/src/main/java/com/erendogan6/havatahminim/room/Converters.kt
@@ -0,0 +1,22 @@
+package com.erendogan6.havatahminim.room
+
+import androidx.room.TypeConverter
+import com.erendogan6.havatahminim.model.DailyForecastBaseResponse
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class Converters {
+
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromDailyForecastBaseResponse(value: DailyForecastBaseResponse): String {
+        return gson.toJson(value)
+    }
+
+    @TypeConverter
+    fun toDailyForecastBaseResponse(value: String): DailyForecastBaseResponse {
+        val type = object : TypeToken<DailyForecastBaseResponse>() {}.type
+        return gson.fromJson(value, type)
+    }
+}
