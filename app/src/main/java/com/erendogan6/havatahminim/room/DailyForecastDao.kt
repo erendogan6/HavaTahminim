a2b727c002c3a6c4ee177638ac3d3713d73d7e74
erendogan6 <erendogan6@hotmail.com>
2024-07-19 02:47:00 +0300

    feat(dao): create DailyForecastDao interface with getForecastByDate and insertForecast methods

---
+++ app/src/main/java/com/erendogan6/havatahminim/room/DailyForecastDao.kt
@@ -0,0 +1,15 @@
+package com.erendogan6.havatahminim.model
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface DailyForecastDao {
+    @Query("SELECT * FROM daily_forecast WHERE date = :date")
+    suspend fun getForecastByDate(date: Long): DailyForecastEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertForecast(forecast: DailyForecastEntity)
+}
