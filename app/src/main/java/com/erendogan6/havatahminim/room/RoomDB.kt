53b905fbf9bca49c94e3e0c19b5cfb14cb6ef7f4
erendogan6 <erendogan6@hotmail.com>
2024-07-19 02:47:36 +0300

    feat(room): update RoomDB to include DailyForecastEntity and DailyForecastDao

--- app/src/main/java/com/erendogan6/havatahminim/room/RoomDB.kt
+++ app/src/main/java/com/erendogan6/havatahminim/room/RoomDB.kt
@@ -2,10 +2,15 @@ package com.erendogan6.havatahminim.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.erendogan6.havatahminim.model.DailyForecastDao
+import com.erendogan6.havatahminim.model.DailyForecastEntity
 import com.erendogan6.havatahminim.model.LocationDao
 import com.erendogan6.havatahminim.model.LocationEntity
 
-@Database(entities = [LocationEntity::class], version = 1)
+@Database(entities = [LocationEntity::class, DailyForecastEntity::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class RoomDB : RoomDatabase() {
     abstract fun locationDao(): LocationDao
+    abstract fun dailyForecastDao(): DailyForecastDao
 }
