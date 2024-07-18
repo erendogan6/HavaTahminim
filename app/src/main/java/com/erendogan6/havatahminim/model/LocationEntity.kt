64945325a6ed75eb0c700588ab954c141ecd839d
erendogan6 <erendogan6@hotmail.com>
2024-07-19 00:42:39 +0300

    feat(model): create LocationEntity class to store last known location

---
+++ app/src/main/java/com/erendogan6/havatahminim/model/LocationEntity.kt
@@ -0,0 +1,11 @@
+package com.erendogan6.havatahminim.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "location")
+data class LocationEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val latitude: Double,
+    val longitude: Double
+)
